7ad0fde8172ef25f211bc473f25eedf8d5fef6d0
Aldayr Ruiz <aldayr.ruiz@opendeusto.es>
2020-11-20 15:51:12 +0100

    Adds Write Node functionality first part

---
+++ OpcUa.ClientWPF/Commands/WriteNodeCommand.cs
@@ -0,0 +1,43 @@
+﻿using Opc.UaFx;
+using Opc.UaFx.Client;
+using OpcUa.ClientWPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace OpcUa.ClientWPF.Commands
+{
+    public class WriteNodeCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private WriteViewModel WriteViewModel { get; set; }
+
+        public WriteNodeCommand(WriteViewModel writeViewModel)
+        {
+            WriteViewModel = writeViewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            using(OpcClient client = new OpcClient(WriteViewModel.Address))
+            {
+                OpcStatus status = client.WriteNode(WriteViewModel.NodeId, parameter);
+
+                if (status.IsGood)
+                {
+                    // Show good message
+                }
+                else
+                {
+                    // Show error message
+                }
+            }
+        }
+    }
+}
